44775968367898b1ae4e83e22625873ad8592e63
2078502328@qq.com <2078502328@qq.com>
2025-10-21 22:26:50 +0800

    new file:   "hot100/\346\234\211\346\225\210\347\232\204\346\213\254\345\217\267/ans.cpp" 	new file:   "hot100/\346\234\211\346\225\210\347\232\204\346\213\254\345\217\267/note.md"

---
+++ hot100/有效的括号/ans.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        unordered_map<char, char> pairs = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        stack<char> stk;
+        for (char ch: s) {
+            if (pairs.count(ch)) {
+                if (stk.empty() || stk.top() != pairs[ch]) {
+                    return false;
+                }
+                stk.pop();
+            }
+            else {
+                stk.push(ch);
+            }
+        }
+        return stk.empty();
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/valid-parentheses/solutions/1/you-xiao-de-gua-hao-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
---
+++ hot100/有效的括号/note.md
@@ -0,0 +1,7 @@
+### #20 有效的括号
+
+学习stack的写法：
+
+- stack<char,string> st(s)来用容器(s)初始化一个stack
+- void push,pop
+- top返回一个指向栈顶的引用
