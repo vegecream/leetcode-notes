e08752f81b24efa628b5202c55b1a5f9248bf7ec
2078502328@qq.com <2078502328@qq.com>
2025-10-27 21:49:07 +0800

    反转链表

---
+++ hot100/反转链表/ans.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL) return head;
+        if(head -> next == NULL) return head;
+        ListNode *p = NULL;
+        ListNode *c = head;
+
+        while(c){
+            ListNode *s = c -> next;
+            c -> next = p;
+            p = c;
+            c = s;
+        }
+
+        return p;
+    }
+};
