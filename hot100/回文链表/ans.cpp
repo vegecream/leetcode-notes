f2d3acefea7032c6a5241fad4972eb4b7b15f7ce
2078502328@qq.com <2078502328@qq.com>
2025-10-28 19:25:31 +0800

    #234

---
+++ hot100/回文链表/ans.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr) {
+            return true;
+        }
+
+        // 找到前半部分链表的尾节点并反转后半部分链表
+        ListNode* firstHalfEnd = endOfFirstHalf(head);
+        ListNode* secondHalfStart = reverseList(firstHalfEnd->next);
+
+        // 判断是否回文
+        ListNode* p1 = head;
+        ListNode* p2 = secondHalfStart;
+        bool result = true;
+        while (result && p2 != nullptr) {
+            if (p1->val != p2->val) {
+                result = false;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+
+        // 还原链表并返回结果
+        firstHalfEnd->next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    ListNode* endOfFirstHalf(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/palindrome-linked-list/solutions/457059/hui-wen-lian-biao-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
